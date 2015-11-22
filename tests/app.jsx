ba482240890f8aef7097ecce83bdc6895ac0d6f1
Sergey Ivanov <xifrin@gmail.com>
2015-11-22 13:51:59 +0600

    WebsocketUI render test

---
+++ tests/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import WebsocketUI from '../src/components/WebsocketUI.jsx'
+import TestUtils from 'react/lib/ReactTestUtils'
+
+describe('WebsocketUI', function () {
+  it('renders without problems', function () {
+    var component = TestUtils.renderIntoDocument(<WebsocketUI />),
+        componentNode = ReactDOM.findDOMNode(component);
+
+    expect(component).not.toBeUndefined()
+    expect(componentNode).not.toBeUndefined()
+    expect(componentNode.textContent).toContain('There\'re no connections')
+  });
+});
