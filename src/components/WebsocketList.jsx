bb355e28a8bed45cd566fda4443adaffbe5adbfa
Sergey Ivanov <xifrin@gmail.com>
2015-11-24 00:07:56 +0600

    Extract WebsocketItem from WebsocketList file

--- src/components/WebsocketList.jsx
+++ src/components/WebsocketList.jsx
@@ -1,60 +1,10 @@
 import _ from 'lodash'
 import React from 'react'
-import MessageForm from './form/MessageForm.jsx'
-import WebsocketMessages from './WebsocketMessages.jsx'
-import WebsocketActions from '../actions/WebsocketActions'
-
-class WebsocketItem extends React.Component {
-  _handleWebsocketClose(e) {
-    e.preventDefault();
-    WebsocketActions.close(this.props.id)
-    return false;
-  }
-  _handleWebsocketDestroy(e) {
-    e.preventDefault();
-    WebsocketActions.destroy(this.props.id)
-    return false;
-  }
-  _onMessageSubmit(message) {
-    WebsocketActions.send(this.props.id, message)
-  }
-  render() {
-    var websocket = this.props.websocket;
-
-    return (
-      <div className="ui inverted segment">
-        <div className="ui attached inverted divided list">
-          <div>
-            {websocket.address}
-          </div>
-          <div>
-            {
-              websocket.isOpen() ?
-                <a href="#" onClick={this._handleWebsocketClose.bind(this)}>[ close ]</a> :
-                <a href="#" onClick={this._handleWebsocketDestroy.bind(this)}>[ destroy ]</a>
-            }
-          </div>
-        </div>
-        {
-          websocket.isOpen() ?
-            <MessageForm onSubmit={this._onMessageSubmit.bind(this)} /> :
-            false
-        }
-
-        <WebsocketMessages messages={websocket.messages} />
-      </div>
-    )
-  }
-}
-
-WebsocketItem.propTypes = {
-  id: React.PropTypes.number,
-  websocket: React.PropTypes.object,
-}
+import WebsocketItem from './WebsocketItem.jsx'
 
 class WebsocketList extends React.Component {
   hasData() {
-    return _.size(this.props.websockets)
+    return _.size(this.props.websockets) > 0
   }
   render() {
     if (this.hasData()) {
